88a654ffbc716c533246572e77b210e92d11a804
Marc Gerald <dagodemarcgeraldarante@gmail.com>
2025-12-18 22:30:32 +0800

    Add authentication module skeleton

---
+++ src/auth.js
@@ -0,0 +1,13 @@
+// Authentication module
+function login(username, password) {
+  // TODO: Implement login logic
+  return false;
+}
+
+function logout() {
+  // TODO: Implement logout logic
+  console.log("User logged out");
+}
+
+export { login, logout };
+
